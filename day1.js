e4b657c29642f33b675ddbfbc7b79c9e047407dc
Deesome <97581463+Deesome@users.noreply.github.com>
2024-07-14 00:30:26 +0530

    Pushing Day 1 challenge

---
+++ day1.js
@@ -0,0 +1,27 @@
+// Activity 1 -- Variable declaration
+var num = 4;
+console.log(num)
+let str = "Deepak";
+console.log(str)
+
+// Activity 2 -- Constant declaration
+const boolean = true;
+
+console.log(boolean);
+
+// Activity - 3 --Data Types
+let value = 4;
+let name1  = "Ironman";
+let isTrue = true;;
+let obj = {};
+let arr = [1,2,3,4,5];
+
+console.log(typeof value)
+console.log(typeof name1)
+console.log(typeof isTrue)
+console.log(typeof obj)
+console.log(typeof arr)
+
+//Activity 4
+let a = 10;
+let a = 9;
